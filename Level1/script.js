eeb66ab80063a570474b46256eb40ccce65034a1
sharanya-25250198 <25250198sharanya@staloysius.ac.in>
2026-05-19 15:17:39 +0530

    Restore Level1 landing page

--- Level1/script.js
+++ Level1/script.js
@@ -1,3 +1,30 @@
-function showMessage() {
-    alert("Welcome to Cognifyz Internship Project!");
+// Color Change Button
+const colorBtn = document.getElementById("colorBtn");
+
+colorBtn.addEventListener("click", function(){
+
+    document.body.style.backgroundColor =
+    "#" + Math.floor(Math.random()*16777215).toString(16);
+
+});
+
+// Greeting Alert
+function showGreeting(){
+
+    alert("Hello! Welcome to Cognifyz Internship Project 🎉");
+
+}
+
+// Calculator
+function calculateSum(){
+
+    let num1 = Number(document.getElementById("num1").value);
+
+    let num2 = Number(document.getElementById("num2").value);
+
+    let sum = num1 + num2;
+
+    document.getElementById("result").innerText =
+    "Result: " + sum;
+
 }
